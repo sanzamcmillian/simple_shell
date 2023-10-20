2e6ca687108a292ce21ded75cb63ded240c4cbc0
sanzamcmillian <g18s2658@ru.ac.za>
2023-10-20 22:19:23 +0200

    new

--- main.c
+++ main.c
@@ -1,110 +1,61 @@
 #include "shell.h"
 
+
+	char **commands = NULL;
+	char *line = NULL;
+	char *shell_name = NULL;
+	int status = 0;
+
 /**
- *main - core of the program
- *@argc: counts the number of commands
- *@argv: stores the commannds in string format
- *@env: the environment with all the executable commands
+ * main - the main shell code
+ * @argc: number of arguments passed
+ * @argv: program arguments to be parsed
  *
- *Return: on success returns (0), otherwise push errors
-**/
+ * applies the functions in utils and helpers
+ * implements EOF
+ * Prints error on Failure
+ * Return: 0 on success
+ */
 
 
-int main(int argc, char *argv[], char **env)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	char *buff = NULL, *prompt = "$ ";
-	size_t buff_size = argc;
-	ssize_t bytes;
-	pid_t wpid;
-	int w_status;
-	int from_pipe = 0;
-	struct stat statbuf;
- 	char *resolved_path = NULL;
- 	char *command_to_run = NULL;;
+	char **current_command = NULL;
+	int i, type_command = 0;
+	size_t n = 0;
 
-    	while(!from_pipe)
-    	{
-        	if (isatty(STDIN_FILENO) == 0)
-			from_pipe = 1;
-
-        	write(STDOUT_FILENO, prompt, 2);
-
-        	/*receive command */
-        	bytes = _getline(&buff, &buff_size, stdin);
-        	if (bytes == -1)
-        	{
-            		perror("Error (getline)");
-			free(buff);
-            		exit(EXIT_FAILURE);
-        	}
-
-        	if (buff[bytes - 1] == '\n')
-            	buff[bytes - 1] = '\0';
-        	argv = split_line(buff);
-
-		if (!argv[0])
+	signal(SIGINT, ctrl_c_handler);
+	shell_name = argv[0];
+	while (1)
+	{
+		non_interactive();
+		print(" ($) ", STDOUT_FILENO);
+		if (getline(&line, &n, stdin) == -1)
 		{
-			free(argv);
-			continue;
+			free(line);
+			exit(status);
 		}
-        	if (strcmp(argv[0], "exit") == 0)
+			remove_newline(line);
+			remove_comment(line);
+			commands = tokenizer(line, ";");
+
+		for (i = 0; commands[i] != NULL; i++)
 		{
-			free(buff);
-           	 	free(argv);
-            		exit(0);
-        	}
-        	if (strcmp(argv[0], "env") == 0 || strcmp(argv[0], "printenv") == 0)
-		{
-            		char **current_env_var = env;
-            		while (*current_env_var)
- 			{
-                		printf("%s\n", *current_env_var);
-                		current_env_var++;
-                	}
-		free(buff);
-            	free(argv);
-            	continue; /* Move to next iteration of the loop to await next command */
+			current_command = tokenizer(commands[i], " ");
+			if (current_command[0] == NULL)
+			{
+				free(current_command);
+				break;
+			}
+			type_command = parse_command(current_command[0]);
+
+			/* initializer -   */
+			initializer(current_command, type_command);
+			free(current_command);
 		}
-
-        	resolved_path = resolve_path(argv[0]);
-        	command_to_run = resolved_path ? resolved_path : argv[0];
-
-        	if (!check_exe(command_to_run, &statbuf))
-        	{
-            		perror("Error (file status)");
-            		free(resolved_path);
-            		free(argv);
-            		continue;
-        	}
-
-        	wpid = fork();
-
-        	if (wpid == -1)
-        	{
-            		perror("Error (fork)");
-			free(buff);
-            		free(argv);
-            		free(resolved_path);
-            		exit(EXIT_FAILURE);
-        	}
-        	if (wpid == 0)
-        	{
-            		execve(command_to_run, argv, env);
-            		perror("Error (execve)");
-            		exit(EXIT_FAILURE);
-        	}
-
-
-        	if (waitpid(wpid, &w_status, 0) == -1)
-        	{
-            		perror("Error (wait)");
-			free(buff);
-            		free(argv);
-            		exit(EXIT_FAILURE);
-        	}
-        	free(argv);
-        	/*free(command_to_run);*/
+		free(commands);
 	}
-    	free(buff);
-    	return (0);
+	free(line);
+
+	return (status);
 }
